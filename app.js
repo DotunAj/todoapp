db0d1d4e6a9947372ec4b66b66d935d07ba8c1fb
Ayodotun <dotunajala@gmail.com>
2019-02-14 00:05:26 +0100

    feat: install express and configure for get request

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("Hurray!");
+const express = require('express');
+const db = require('./db/db');
+
+// Set up the express app
+const app = express();
+
+// Get all todos
+app.get('/api/v1/todos', (req, res) => {
+    res.status(200).send({
+        success: 'true',
+        message: 'todos retrieved successfully',
+        todos: db
+    })
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`)
+});
