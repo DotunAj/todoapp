9fe65d459e25a75ae8a7567fa4aa212977202299
Ayodotun <dotunajala@gmail.com>
2019-02-20 02:48:51 +0100

    Initialize sequelize to work with postgresql

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const router = require('./routes/index')
+const router = require('./routes/index');
+
+// Setting up db connection
+const db = require('./config/database');
+
+// Testing db
+db
+    .authenticate()
+    .then(() => console.log('Database connection has been established successfully...'))
+    .catch((err) => console.log('Error', err) );
 
 // Set up the express app
 const app = express();
@@ -11,7 +20,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(router);
 
 // Start server on port 5000
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`)
+    console.log(`server running on port ${PORT}`);
 });
